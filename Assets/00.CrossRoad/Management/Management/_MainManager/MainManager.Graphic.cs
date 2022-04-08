e86eb0e558135ce3615da0e11fbd03117031a979
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2022-04-09 08:12:51 +0900

    DimView camera control update
    치수선 촬영을 위해 배치한 카메라 촬영단계 종료시 삭제 추가

---
+++ Assets/00.CrossRoad/Management/Management/_MainManager/MainManager.Graphic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Management
+{
+	using UnityEngine.Rendering.Universal;
+
+	/// <summary>
+	/// 코어의 그래픽 관리 코드
+	/// </summary>
+	public partial class MainManager : IManager<MainManager>
+	{
+		public void Graphic_ToggleOutline(bool isOn)
+		{
+			ForwardRendererData rSetting = RenderSetting;
+
+			rSetting.rendererFeatures[0].SetActive(isOn);
+		}
+
+		public void Graphic_ToggleOutline()
+		{
+			ForwardRendererData rSetting = RenderSetting;
+			
+			rSetting.rendererFeatures[0].SetActive(!rSetting.rendererFeatures[0].isActive);
+			//Debug.Log($"{rSetting.rendererFeatures[0].name}");	// NewOutlineFeature
+		}
+	}
+}
