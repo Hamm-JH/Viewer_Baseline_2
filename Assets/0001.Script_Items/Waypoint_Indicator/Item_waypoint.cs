a8f0c6bb526b3fcb00a6fc36ea0cad319c818aaf
JH Hamm <rimmoyee@gmail.com>
2022-05-17 15:35:40 +0900

    0.2.4 update Waypoint Indicator sample color change

---
+++ Assets/0001.Script_Items/Waypoint_Indicator/Item_waypoint.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Items
+{
+    public class Item_waypoint : MonoBehaviour
+    {
+        [System.Serializable]
+        public class _IssueWayPoint
+        {
+            public Waypoint_Indicator mainIcon_waypoint;
+            public Waypoint_Indicator effect_waypoint;
+
+            public bool IsNotNull()
+            {
+                bool result = false;
+
+                if (mainIcon_waypoint != null && effect_waypoint != null) result = true;
+
+                return result;
+            }
+
+            public void SetColor(Color _color)
+            {
+                mainIcon_waypoint.onScreenSpriteColor = Color.white;
+                mainIcon_waypoint.offScreenSpriteColor = Color.white;
+                effect_waypoint.onScreenGameObjectColor = Color.white;
+                effect_waypoint.offScreenGameObjectColor = Color.white;
+            }
+        }
+
+        [SerializeField]
+        private _IssueWayPoint m_issueWayPoint;
+
+        public _IssueWayPoint IssueWayPoint { get => m_issueWayPoint; set => m_issueWayPoint = value; }
+
+        //private void Start()
+        //{
+        //    if (IssueWayPoint != null)
+        //    {
+        //        IssueWayPoint.SetColor(Color.white);
+        //    }
+        //}
+
+        public void SetColor(Color _color)
+        {
+            if(IssueWayPoint.IsNotNull())
+            {
+                IssueWayPoint.SetColor(_color);
+            }
+        }
+    }
+}
