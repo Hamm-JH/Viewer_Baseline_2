df0edf75a1208c2ea455d5dc4e284865c13af4b7
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2022-06-12 23:24:49 +0900

    0.2.31 Update
    Voxel Test Code Update, Prepare 1st Demonstration

---
+++ Assets/0002.New module/Voxel/New Folder/Voxel.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Test.Voxelizer;
+
+namespace Test
+{
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(BoxCollider))]
+    public class Voxel : MonoBehaviour
+    {
+        [System.Serializable]
+        public class VoxelCache
+        {
+            [SerializeField] Mesh m_mesh;
+            [SerializeField] Material m_mat;
+            [SerializeField] MeshFilter m_filter;
+            [SerializeField] MeshRenderer m_renderer;
+            [SerializeField] BoxCollider m_boxCollider;
+            //[SerializeField] Rigidbody m_rigidbody;
+
+            public Mesh _Mesh { get => m_mesh; set => m_mesh = value; }
+            public Material _Mat { get => m_mat; set => m_mat = value; }
+            public MeshFilter _Filter { get => m_filter; set => m_filter = value; }
+            public MeshRenderer _Renderer { get => m_renderer; set => m_renderer = value; }
+            public BoxCollider _BoxCollider { get => m_boxCollider; set => m_boxCollider = value; }
+            //public Rigidbody _Rigidbody { get => m_rigidbody; set => m_rigidbody = value; }
+
+            [SerializeField] bool m_isCollision;
+            public bool IsCollision 
+            { 
+                get => m_isCollision; 
+                set
+                {
+                    m_isCollision = value;
+
+                    if (value == true)
+                    {
+                        _Renderer.material.color = Color.red;
+                    }
+                    else
+                    {
+                        Color colr = new Color(0xFB / 255f, 1, 0, 0.1f);
+                        _Renderer.material.color = colr;
+                    }
+                }
+            }
+
+            
+        }
+
+        // 단일 복셀 처리
+        [SerializeField] VoxelCache m_cache;
+
+        #region Init
+        public void InitVoxel(string _name, Vector3 _center, Vector3 _scale, Mesh _mesh, Material _mat)
+        {
+            m_cache = new VoxelCache();
+
+            gameObject.name = _name;
+            //m_cache._Rigidbody = gameObject.AddComponent<Rigidbody>();
+
+            m_cache._Mesh = _mesh;
+            m_cache._Mat = _mat;
+
+            m_cache._Filter = gameObject.GetComponent<MeshFilter>();
+            m_cache._Filter.mesh = _mesh;
+            m_cache._Renderer = gameObject.GetComponent<MeshRenderer>();
+            m_cache._BoxCollider = gameObject.GetComponent<BoxCollider>();
+
+            SetMesh(m_cache._Filter, _mesh);
+            SetMaterial(m_cache._Renderer, _mat);
+            SetColliderScale(m_cache._BoxCollider, _scale);
+            //SetRigidBody(m_cache._Rigidbody);
+
+            SetCenter(_center);
+
+            m_cache.IsCollision = false;
+        }
+
+        private void SetMesh(MeshFilter _filter, Mesh _mesh)
+        {
+            _filter.mesh = _mesh;
+        }
+
+        private void SetMaterial(MeshRenderer _renderer, Material _mat)
+        {
+            _renderer.material = _mat;
+        }
+
+        private void SetColliderScale(BoxCollider _collider, Vector3 _scale)
+        {
+            _collider.size = _scale;
+        }
+
+        private void SetRigidBody(Rigidbody _rigid)
+        {
+            _rigid.useGravity = false;
+            _rigid.isKinematic = false;
+            _rigid.mass = 0;
+            _rigid.angularDrag = 0;
+            _rigid.constraints = RigidbodyConstraints.FreezeAll;
+        }
+
+        private void SetCenter(Vector3 _center)
+        {
+            transform.position = _center;
+        }
+        #endregion
+
+        //private void OnCollisionEnter(Collision collision)
+        //{
+        //    Debug.Log(collision.collider.name);
+        //    if (collision.collider.name == "Cube")
+        //    {
+        //        Debug.Log("Hello");
+        //        m_cache.IsCollision = true;
+        //    }
+        //}
+
+        //private void OnCollisionStay(Collision collision)
+        //{
+        //    //if(collision.collider.name == "HitData")
+        //    //{
+        //    //    Debug.Log("Hello");
+        //    //    m_cache.IsCollision = true;
+        //    //}
+        //}
+
+        //private void OnCollisionExit(Collision collision)
+        //{
+        //    if (collision.collider.name == "Cube")
+        //        m_cache.IsCollision = false;
+        //}
+    }
+}
---
+++ Assets/0002.New module/Voxel/New Folder/Voxelizer.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class Voxelizer : MonoBehaviour
+    {
+        public enum PlacePosition
+        {
+            /// <summary>
+            /// 원점을 배치기준으로 잡는 방법
+            /// </summary>
+            Original,
+
+            /// <summary>
+            /// 중심점을 배치기준으로 잡는 방법
+            /// </summary>
+            Center,
+        }
+
+        public PlacePosition _PlacePosition;
+        public Vector3 _scale;
+
+        public Material m_mat;
+        public Mesh m_mesh;
+
+        public GameObject m_root;
+
+        private void Start()
+        {
+            m_mesh = InstantiateMesh(_scale, _PlacePosition);
+
+            m_root = new GameObject("voxel root");
+
+            for (int x = 0; x < 12; x++)
+            {
+                for (int y = 0; y < 12; y++)
+                {
+                    for (int z = 0; z < 12; z++)
+                    {
+                        GameObject voxel = CreateVoxel(new int[3] { x, y, z }, _scale, m_mesh, m_mat);
+                        voxel.transform.SetParent(m_root.transform);
+                    }
+                }
+            }
+        }
+
+        public GameObject CreateVoxel(int[] _indexes, Vector3 _scale, Mesh _instantiatedMesh, Material _mat)
+        {
+            Vector3 center = new Vector3(
+                _scale.x * _indexes[0],
+                _scale.y * _indexes[1],
+                _scale.z * _indexes[2]
+                );
+
+            GameObject obj = new GameObject();
+
+            Mesh mesh = Instantiate<Mesh>(_instantiatedMesh);
+            Voxel voxel = obj.AddComponent<Voxel>();
+
+            voxel.InitVoxel(
+                $"Voxel {_indexes[0]},{_indexes[1]},{_indexes[2]}",
+                center, _scale, mesh, _mat);
+
+            return obj;
+        }
+
+        // 복셀 배치
+        public Mesh InstantiateMesh(Vector3 _scale, PlacePosition _place)
+        {
+            Vector3 pPos = new Vector3(
+                _place == PlacePosition.Center ? -(_scale.x / 2) : 0,
+                _place == PlacePosition.Center ? -(_scale.y / 2) : 0,
+                _place == PlacePosition.Center ? -(_scale.z / 2) : 0);
+
+            Mesh mesh = new Mesh();
+            List<Vector3> verts = new List<Vector3>();
+            verts.Add(new Vector3(pPos.x + 0        , pPos.y + 0        , pPos.z + 0));
+            verts.Add(new Vector3(pPos.x + _scale.x , pPos.y + 0        , pPos.z + 0));
+            verts.Add(new Vector3(pPos.x + 0        , pPos.y + 0        , pPos.z + _scale.z));
+            verts.Add(new Vector3(pPos.x + _scale.x , pPos.y + 0        , pPos.z + _scale.z));
+
+            verts.Add(new Vector3(pPos.x + 0        , pPos.y + _scale.y , pPos.z + 0));
+            verts.Add(new Vector3(pPos.x +_scale.x  , pPos.y + _scale.y , pPos.z + 0));
+            verts.Add(new Vector3(pPos.x + 0        , pPos.y + _scale.y , pPos.z + _scale.z));
+            verts.Add(new Vector3(pPos.x +_scale.x  , pPos.y + _scale.y , pPos.z + _scale.z));
+            mesh.vertices = verts.ToArray();
+
+            List<int> tris = new List<int>();
+            tris.AddRange(new List<int>() { 0, 1, 2 });
+            tris.AddRange(new List<int>() { 3, 2, 1 });
+
+            tris.AddRange(new List<int>() { 0, 4, 1 });
+            tris.AddRange(new List<int>() { 5, 1, 4 });
+
+            tris.AddRange(new List<int>() { 1, 5, 3 });
+            tris.AddRange(new List<int>() { 7, 3, 5 });
+
+            tris.AddRange(new List<int>() { 7, 6, 3 });
+            tris.AddRange(new List<int>() { 2, 3, 6 });
+
+            tris.AddRange(new List<int>() { 6, 4, 2 });
+            tris.AddRange(new List<int>() { 0, 2, 4 });
+
+            tris.AddRange(new List<int>() { 4, 6, 5 });
+            tris.AddRange(new List<int>() { 7, 5, 6 });
+            mesh.triangles = tris.ToArray();
+
+            return mesh;
+        }
+    }
+}
